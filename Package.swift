614b08315490a8be3afe6723c711707139cb8084
Borealin <41241077+Borealin@users.noreply.github.com>
2026-01-12 12:01:40 +0000

    Release v137.7151.12

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AGC2Capi",
-            url: "https://github.com/Entertech/webrtc-audio-processing-build/releases/download/v0.0.0-pre/AGC2Capi.xcframework.zip",
-            checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+            url: "https://github.com/Entertech/webrtc-audio-processing-build/releases/download/v137.7151.12/AGC2Capi.xcframework.zip",
+            checksum: "4669ac2cfbf97376257257039c39d5b7337d32c4b034931914894920954d6ec3"
         )
     ]
 )
