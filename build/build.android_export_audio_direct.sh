61826ba2899d9db6351f593e01fff1d77693a86f
Borealin <me@borealin.cn>
2026-01-16 13:04:09 +0800

    fix: fix path

--- build/build.android_export_audio_direct.sh
+++ build/build.android_export_audio_direct.sh
@@ -7,10 +7,14 @@ SCRIPT_DIR="`pwd`"
 PACKAGE_NAME=android_export_audio
 COMMIT="${1:-}"
 
-SCRIPT_DIR/scripts/apt_install_x86_64.sh \
+./scripts/apt_install_x86_64.sh \
   && sudo apt-get install -y openjdk-11-jdk \
   && sudo apt-get install -y build-essential
 
 python3 run.py build $PACKAGE_NAME --commit "${COMMIT}" --shallow
-export PATH="$PWD/_source/$PACKAGE_NAME/depot_tools:$PATH"
+echo "Build completed"
+echo "depot_tools exists: $(ls -la $SCRIPT_DIR/_source/$PACKAGE_NAME/depot_tools)"
+export PATH="$PATH:$SCRIPT_DIR/_source/$PACKAGE_NAME/depot_tools"
+echo "PATH: $PATH"
 python3 run.py package $PACKAGE_NAME
+echo "Package completed"
