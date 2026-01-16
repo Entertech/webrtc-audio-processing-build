fe60cc9cdbe4c51e5757150bc99b190089ea9fbe
Borealin <me@borealin.cn>
2026-01-16 14:54:32 +0800

    fix: fix upstream version

--- build/build.android_export_audio_direct.sh
+++ build/build.android_export_audio_direct.sh
@@ -7,14 +7,12 @@ SCRIPT_DIR="`pwd`"
 PACKAGE_NAME=android_export_audio
 COMMIT="${1:-}"
 
+set -ex
+
 sudo ./scripts/apt_install_x86_64.sh \
   && sudo apt-get install -y openjdk-11-jdk \
   && sudo apt-get install -y build-essential
 
 python3 run.py build $PACKAGE_NAME --commit "${COMMIT}" --shallow
-echo "Build completed"
-echo "depot_tools exists: $(ls -la $SCRIPT_DIR/_source/$PACKAGE_NAME/depot_tools)"
 export PATH="$PATH:$SCRIPT_DIR/_source/$PACKAGE_NAME/depot_tools"
-echo "PATH: $PATH"
 python3 run.py package $PACKAGE_NAME
-echo "Package completed"
